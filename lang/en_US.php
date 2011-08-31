4417059eb0da9f69687f645450b8a4eab2e129a8
Aaron Carlino <unclecheese@leftandmain.com>
2011-08-31 15:08:41 -0400

    MINOR: Added lang folder

---
+++ lang/en_US.php
@@ -0,0 +1,36 @@
+<?php
+
+global $lang;
+
+$lang['en_US']['FileAttachmentField']['AREYOUSURE'] = 'Are you sure you want to delete this file permanently?';
+$lang['en_US']['FileAttachmentField']['CANCEL'] = 'Cancel';
+$lang['en_US']['FileAttachmentField']['DELETEFROMFILES'] = 'Delete from files';
+$lang['en_US']['FileAttachmentField']['DELETEPERMANENTLY'] = 'Delete permanently';
+$lang['en_US']['FileAttachmentField']['DETACH'] = 'Detach';
+$lang['en_US']['FileAttachmentField']['DROPFILESHERE'] = 'Drop files here';
+$lang['en_US']['FileAttachmentField']['EDIT'] = 'Edit';
+$lang['en_US']['FileAttachmentField']['FROMFILES'] = 'From files';
+$lang['en_US']['FileAttachmentField']['FROMYOURCOMPUTER'] = 'From your computer';
+$lang['en_US']['FileAttachmentField']['NOFILESATTACHED'] = 'No files attached';
+$lang['en_US']['FileAttachmentField']['REMOVE'] = 'Remove';
+$lang['en_US']['FileAttachmentField']['REPLACE'] = 'Replace';
+$lang['en_US']['FileAttachmentField']['UPLOADFILES'] = 'Upload files';
+$lang['en_US']['KickAssetAdmin']['ATTACHFILES'] = 'Attach selected file(s)';
+$lang['en_US']['KickAssetAdmin']['DELETEFILES'] = 'Are you sure you want to delete the selected file(s)?';
+$lang['en_US']['KickAssetAdmin']['GALLERY'] = 'Gallery';
+$lang['en_US']['KickAssetAdmin']['GRID'] = 'Grid';
+$lang['en_US']['KickAssetAdmin']['NEWFOLDER'] = 'New Folder';
+$lang['en_US']['KickAssetAdmin']['VIEW'] = 'View';
+$lang['en_US']['KickAssets']['FILENAME'] = 'Filename';
+$lang['en_US']['KickAssets']['MOVETOFOLDER'] = 'Move to folder';
+$lang['en_US']['KickAssets']['NAME'] = 'Name';
+$lang['en_US']['KickAssets']['NONE'] = 'None';
+$lang['en_US']['KickAssets']['OWNER'] = 'Owner';
+$lang['en_US']['KickAssets']['PERMISSIONFAIL'] = 'You do not have permission to access this section of the CMS.';
+$lang['en_US']['KickAssets']['PLEASEUPLOADONEFILE'] = 'Please upload only one file.';
+$lang['en_US']['KickAssets']['REPLACEFILE'] = 'Replace file';
+$lang['en_US']['KickAssets']['SIZE'] = 'Size';
+$lang['en_US']['KickAssets']['TITLE'] = 'Title';
+$lang['en_US']['KickAssetts']['CANCEL'] = 'Cancel';
+$lang['en_US']['KickAssetts']['SAVE'] = 'Save';
+
