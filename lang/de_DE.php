87efde8483c53c50be7c0b70e3c5bb5adfd82475
Aaron Carlino <unclecheese@leftandmain.com>
2011-09-08 10:25:19 -0400

    MINOR: Italian, German

---
+++ lang/de_DE.php
@@ -0,0 +1,36 @@
+<?php
+
+global $lang;
+
+$lang['de_DE']['FileAttachmentField']['AREYOUSURE'] = 'Sollen wirklich alle Dateien dauerhaft gelöscht werden?';
+$lang['de_DE']['FileAttachmentField']['CANCEL'] = 'Abbrechen';
+$lang['de_DE']['FileAttachmentField']['DELETEFROMFILES'] = 'Lösche Dateien';
+$lang['de_DE']['FileAttachmentField']['DELETEPERMANENTLY'] = 'Löschen';
+$lang['de_DE']['FileAttachmentField']['DETACH'] = 'Trennen';
+$lang['de_DE']['FileAttachmentField']['DROPFILESHERE'] = 'Dateien hier ablegen';
+$lang['de_DE']['FileAttachmentField']['EDIT'] = 'Bearbeiten';
+$lang['de_DE']['FileAttachmentField']['FROMFILES'] = 'Aus Dateien';
+$lang['de_DE']['FileAttachmentField']['FROMYOURCOMPUTER'] = 'Vom Computer';
+$lang['de_DE']['FileAttachmentField']['NOFILESATTACHED'] = 'Keine Dateien angehängt';
+$lang['de_DE']['FileAttachmentField']['REMOVE'] = 'Löschen';
+$lang['de_DE']['FileAttachmentField']['REPLACE'] = 'Ersetzen';
+$lang['de_DE']['FileAttachmentField']['UPLOADFILES'] = 'Dateien hochladen';
+$lang['de_DE']['KickAssetAdmin']['ATTACHFILES'] = 'Ausgewählte Datei(en) anhängen';
+$lang['de_DE']['KickAssetAdmin']['DELETEFILES'] = 'Sollen alle ausgewählten Dateien gelöscht werden?';
+$lang['de_DE']['KickAssetAdmin']['GALLERY'] = 'Galerie';
+$lang['de_DE']['KickAssetAdmin']['GRID'] = 'Raster';
+$lang['de_DE']['KickAssetAdmin']['NEWFOLDER'] = 'Neuer Ordner';
+$lang['de_DE']['KickAssetAdmin']['VIEW'] = 'Ansehen';
+$lang['de_DE']['KickAssets']['FILENAME'] = 'Dateiname';
+$lang['de_DE']['KickAssets']['MOVETOFOLDER'] = 'In Ordner verschieben';
+$lang['de_DE']['KickAssets']['NAME'] = 'Name';
+$lang['de_DE']['KickAssets']['NONE'] = 'Keine(r)';
+$lang['de_DE']['KickAssets']['OWNER'] = 'Besitzer';
+$lang['de_DE']['KickAssets']['PERMISSIONFAIL'] = 'Sie haben keine Berechtigung diese Sektion des CMS aufzurufen.';
+$lang['de_DE']['KickAssets']['PLEASEUPLOADONEFILE'] = 'Bitte nur eine Datei hochladen.';
+$lang['de_DE']['KickAssets']['REPLACEFILE'] = 'Datei ersetzen';
+$lang['de_DE']['KickAssets']['SIZE'] = 'Größe';
+$lang['de_DE']['KickAssets']['TITLE'] = 'Titel';
+$lang['de_DE']['KickAssetts']['CANCEL'] = 'Abbrechen';
+$lang['de_DE']['KickAssetts']['SAVE'] = 'Speichern';
+
---
+++ lang/it_IT.php
@@ -0,0 +1,36 @@
+<?php
+
+global $lang;
+
+$lang['it_IT']['FileAttachmentField']['AREYOUSURE'] = 'Sei sicuro di voler cancellare questo file permanentemente?';
+$lang['it_IT']['FileAttachmentField']['CANCEL'] = 'Annulla';
+$lang['it_IT']['FileAttachmentField']['DELETEFROMFILES'] = 'Elimina dall\'archivio file';
+$lang['it_IT']['FileAttachmentField']['DELETEPERMANENTLY'] = 'Elimina permanentemente';
+$lang['it_IT']['FileAttachmentField']['DETACH'] = 'Scollega';
+$lang['it_IT']['FileAttachmentField']['DROPFILESHERE'] = 'Trascina i file qui';
+$lang['it_IT']['FileAttachmentField']['EDIT'] = 'Modifica';
+$lang['it_IT']['FileAttachmentField']['FROMFILES'] = 'Dall\'archivio file';
+$lang['it_IT']['FileAttachmentField']['FROMYOURCOMPUTER'] = 'Dal tuo computer';
+$lang['it_IT']['FileAttachmentField']['NOFILESATTACHED'] = 'Nessun file allegato';
+$lang['it_IT']['FileAttachmentField']['REMOVE'] = 'Rimuovi';
+$lang['it_IT']['FileAttachmentField']['REPLACE'] = 'Sostituisci';
+$lang['it_IT']['FileAttachmentField']['UPLOADFILES'] = 'Carica file';
+$lang['it_IT']['KickAssetAdmin']['ATTACHFILES'] = 'Allega i file selezionati';
+$lang['it_IT']['KickAssetAdmin']['DELETEFILES'] = 'Sei sicuro di voler eliminare i file selezionati?';
+$lang['it_IT']['KickAssetAdmin']['GALLERY'] = 'Galleria';
+$lang['it_IT']['KickAssetAdmin']['GRID'] = 'Elenco';
+$lang['it_IT']['KickAssetAdmin']['NEWFOLDER'] = 'Nuova cartella';
+$lang['it_IT']['KickAssetAdmin']['VIEW'] = 'Mostra';
+$lang['it_IT']['KickAssets']['FILENAME'] = 'Nome file';
+$lang['it_IT']['KickAssets']['MOVETOFOLDER'] = 'Muovi nella cartella';
+$lang['it_IT']['KickAssets']['NAME'] = 'Nome';
+$lang['it_IT']['KickAssets']['NONE'] = 'Nessuno';
+$lang['it_IT']['KickAssets']['OWNER'] = 'Proprietario';
+$lang['it_IT']['KickAssets']['PERMISSIONFAIL'] = 'Non hai i permessi per accedere a questa sezione del CMS.';
+$lang['it_IT']['KickAssets']['PLEASEUPLOADONEFILE'] = 'Per favore, carica solo un file.';
+$lang['it_IT']['KickAssets']['REPLACEFILE'] = 'Sostituisci file';
+$lang['it_IT']['KickAssets']['SIZE'] = 'Dimensione';
+$lang['it_IT']['KickAssets']['TITLE'] = 'Titolo';
+$lang['it_IT']['KickAssetts']['CANCEL'] = 'Annulla';
+$lang['it_IT']['KickAssetts']['SAVE'] = 'Salva';
+
